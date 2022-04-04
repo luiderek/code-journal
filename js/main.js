448cea034c6a033909c88d952a834fac32c38275
Derek Lui <luiderek@outlook.com>
2022-04-04 18:51:32 +0000

    photoURL placeholder and functionality

--- js/main.js
+++ js/main.js
@@ -1,2 +1,12 @@
 /* global data */
 /* exported data */
+
+const $photoURL = document.querySelector('input[name=photoURL]');
+const $photoPreview = document.querySelector('.photo-preview');
+$photoURL.addEventListener('input', function (e) {
+  $photoPreview.setAttribute('src', e.target.value);
+});
+
+// function l(logtarget) {
+//   console.log(logtarget + ':', logtarget);
+// }
