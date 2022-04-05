158a611a5a30f794010214f2d13e5301df3fafa5
Derek Lui <luiderek@outlook.com>
2022-04-05 00:11:26 +0000

    filling dom with entries

--- js/main.js
+++ js/main.js
@@ -25,3 +25,50 @@ $entryForm.addEventListener('submit', function (e) {
   $photoPreview.setAttribute('src', './images/placeholder-image-square.jpg');
   $entryForm.reset();
 });
+
+function entryToDOM(entry) {
+  // <li>
+  //   <div class="row margin-bot">
+  //     <div class="column-half">
+  //       <img src="https://picsum.photos/2000" alt="">
+  //     </div>
+  //     <div class="column-half">
+  //       <h3>Javascript</h3>
+  //       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio reiciendis mollitia quidem,
+  //         perspiciatis reprehenderit modi dolorem nesciunt vero neque nostrum! Reiciendis repudiandae quibusdam
+  //         officia qui, ut veniam dolores! Iure, ea.</p>
+  //     </div>
+  //   </div>
+  // </li>
+
+  const $li = document.createElement('li');
+  const $divRMb = document.createElement('div');
+  $divRMb.classList.add('row', 'margin-bot');
+  const $img = document.createElement('img');
+  $img.setAttribute('src', entry.photoURL);
+  const $divch1 = document.createElement('div');
+  $divch1.classList.add('column-half');
+  const $divch2 = document.createElement('div');
+  $divch2.classList.add('column-half');
+  const $h3 = document.createElement('h3');
+  $h3.textContent = entry.title;
+  const $p = document.createElement('p');
+  $p.textContent = entry.notes;
+
+  $li.appendChild($divRMb);
+  $divRMb.appendChild($divch1);
+  $divch1.appendChild($img);
+  $divRMb.appendChild($divch2);
+  $divch2.appendChild($h3);
+  $divch2.appendChild($p);
+
+  return $li;
+}
+
+const $entryList = document.querySelector('.entry-view-container');
+
+window.addEventListener('DOMContentLoaded', function (e) {
+  for (const eachentry of data.entries) {
+    $entryList.appendChild(entryToDOM(eachentry));
+  }
+});
